10fe10e00d95e651ed2b628158141c4e5389217d
AaronEGH16 <aezegonz@gmail.com>
2023-06-14 17:24:43 -0700

    print text in screen with printf command

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - print text with printf command
+ *
+ * return: Always return 0
+ */
+
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
