04ab5842265ec5424dbf918b01e3d7dac24f021d
AaronEGH16 <aezegonz@gmail.com>
2023-06-15 09:56:09 -0700

    prints abecedary

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints  abecedary
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	for(char letter = a; letter < z; letter++)
+	{
+		putchar(letter);
+	}
+	return (0);
+}
