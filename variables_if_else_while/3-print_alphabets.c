a0119ece943db4aa495d2627aa155d4f13893532
AaronEGH16 <aezegonz@gmail.com>
2023-06-15 10:25:54 -0700

    print abecedary in upper and lower cases

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the abecedary in upper and lower cases
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	char ol, dl, OL, DL;
+	dl = 'z';
+	DL = 'Z';
+
+	for (ol = 'a'; ol <= dl; ol++)
+		putchar(ol);
+	for (OL = 'A'; OL <= DL; OL++)
+		putchar(OL);
+	printf("\n");
+}
