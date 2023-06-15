39c54394248f57330c25eddb883f9d98ab94f0e0
AaronEGH16 <aezegonz@gmail.com>
2023-06-15 16:46:04 -0700

    prints all number in base 10 from 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers in base 10 from 0 to 10
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int d = 10;
+
+	for (int o = 0; o < d ; o++)
+	{
+		putchar(o);
+	}
+	putchar('\n');
+	return (0);
+}
