bb9e773a12234c42bb5ffc8f1b62a1539b0463cd
AaronEGH16 <aezegonz@gmail.com>
2023-06-16 05:28:45 -0700

    prints number by 0 to 9 using char variable

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	char num;
+	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(num);
+		putchar(48 + num);
 	}
 	putchar('\n');
 	return (0);
