b8a70e8b6541577d30eaac9b8b6b56060a5bfc21
AaronEGH16 <aezegonz@gmail.com>
2023-06-16 09:26:32 -0700

    print numbers by 0 to 9 with ', '

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers by 0 to 9 with ", "
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 9; num++)
+	{
+		putchar (num);
+		putchar (',');
+		putchar (' ');
+	}
+	putchar ('9');
+	putchar ('\n');
+	return (0);
+}
