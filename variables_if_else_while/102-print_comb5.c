360c1db8c1fb5290f17795a10d7886469bfd8df7
AaronEGH16 <aezegonz@gmail.com>
2023-06-16 16:48:06 -0700

    combinations of 2 digits and 2 digits

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - prints a combination of 3 digits (012=120=102=...) followed for ,
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int num1, num2, num3, num4;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		num2 = num1 + 1;
+
+		for (; num2 <= 9; num2++)
+		{
+			num3 = num2 + 1;
+
+			for (; num3 <= 9; num3++)
+			{
+				num4 = num3 + 1;
+
+				for (; num4 <= 9; num4++)
+				{
+					putchar(48 + num1);
+					putchar(48 + num2);
+					putchar(' ');
+					putchar(48 + num3);
+					putchar(48 + num4);
+					if (num1 == 7 && num2 == 8 && num3 == 9)
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
