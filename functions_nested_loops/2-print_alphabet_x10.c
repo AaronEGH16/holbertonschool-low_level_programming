30934cdda20c443920cc0da0a25d2648e99652c0
AaronEGH16 <aezegonz@gmail.com>
2023-06-18 10:36:56 -0700

    call print aplhabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "./main.h"
+
+/**
+ * print_alphabet_x10 - call print alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int x = 0; x <= 10; x++)
+	{
+		print_alphabet();
+	}
+}
