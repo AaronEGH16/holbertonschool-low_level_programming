91e9debb90655fad5c25d507dcac09cf0ad49914
AaronEGH16 <aezegonz@gmail.com>
2023-06-18 12:25:45 -0700

    says if char is lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "./main.h"
+
+/**
+ * _islower - says if c is lower or not
+ *
+ * int C: char to check
+ *
+ * Return: returns != 0 if is lower and == 0 if is´n lower
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
