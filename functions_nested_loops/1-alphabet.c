e5dd913fe23588f17fd4f12b1117e97fd6d37716
AaronEGH16 <aezegonz@gmail.com>
2023-06-18 10:06:15 -0700

    prints alphabet with include funtions

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "./main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ */
+
+void print_alphabet(void)
+{
+	char Origin = 'a';
+
+	for (; Origen <= 'z'; Origin++)
+	{
+		_putchar(Origin);
+	}
+}
