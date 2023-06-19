76d2ad5556dec0870d6be64781c62d0479aab703
AaronEGH16 <aezegonz@gmail.com>
2023-06-19 05:43:29 -0700

    prints all minutes of the day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "./main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	char hr, mn;
+
+	for (hr = "0"; hr <= "23"; hr++)
+	{
+		for (mn = "0"; mn <= "59"; mn++)
+		{
+			_putchar(hr / "10");
+			_putchar(hr % "10");
+			_putchar(mn / "10");
+			_putchar(mn % "10");
+		}
+	}
