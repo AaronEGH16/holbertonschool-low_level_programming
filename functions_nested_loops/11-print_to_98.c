555e7e5ebab1668c8bfcfbf4c5c7273d1f8bf1b0
AaronEGH16 <aezegonz@gmail.com>
2023-06-19 08:31:25 -0700

    prints from N to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,23 @@
 
 void print_to_98(int n)
 {
-	for (; n<=98; n++)
+	for (; n <= 98; n++)
 	{
-		if (n / 10 == 0)
+		if (n / 10 != 0)
+		{
+			_putchar('0' + (n / 10));
+		}
+		_putchar('0' + (n % 10));
+		if (n == 98)
+		{
+			continue;
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (; n >= 98; n--)
+	{
+		if (n / 10 != 0)
 		{
 			_putchar('0' + (n / 10));
 		}
