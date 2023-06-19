a99910951b5b24f9ebb1681c3810ba41bae4bf6d
AaronEGH16 <aezegonz@gmail.com>
2023-06-19 06:10:47 -0700

    prints all minutes of the day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,18 +7,18 @@
 void jack_bauer(void)
 {
 	int hr, mn;
-	char HR1, HR2, MN1, MN2;
 
 	for (hr = 0; hr <= 23; hr++)
 	{
 		for (mn = 0; mn <= 59; mn++)
 		{
-			HR1 = hr / 10;
-			HR2 = hr % 10;
-			MN1 = mn / 10;
-			MN2 = mn % 10;
+			HR1 = '0' + (hr / 10);
+			HR2 = '0' + (hr % 10);
+			MN1 = '0' + (mn / 10);
+			MN2 = '0' + (mn % 10);
 			_putchar(HR1);
 			_putchar(HR2);
+			_putchar(':');
 			_putchar(MN1);
 			_putchar(MN2);
 			_putchar('\n');
