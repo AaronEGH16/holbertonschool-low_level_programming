4a1d27a1e32ebd11d12f7fba0e2cb48aa192dc1c
AaronEGH16 <aezegonz@gmail.com>
2023-06-19 08:25:48 -0700

    prints from N to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "./main.h"
+
+/**
+ * print_to_98 - print from N to 98
+ *
+ * @n: number to start count
+ */
+
+void print_to_98(int n)
+{
+	for (; n<=98; n++)
+	{
+		if (n / 10 == 0)
+		{
+			_putchar('0' + (n / 10));
+		}
+		_putchar('0' + (n % 10));
+		if (n == 98)
+		{
+			continue;
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+}
