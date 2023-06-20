96f1f6cdd317df0212d48a19601e5299cc2690e1
AaronEGH16 <aezegonz@gmail.com>
2023-06-20 05:52:15 -0700

    print from 0 to 100 but multiply of 3 = fizz, multiply of 5 = Buzz and multiply of both = FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 100, but for multiply of 3 print Fizz,
+ *		multiply of 5 print Buzz and for multiply of both print FizzBuzz
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100)
+	{
+		if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (x % 5 != 0 && x % 3 != 0)
+		{
+			printf("%d", x);
+		}
+	}
+}
