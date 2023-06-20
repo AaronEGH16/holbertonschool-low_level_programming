3237aafeeebcd7c2c9e5a5e86e10618f5a14ac5b
AaronEGH16 <aezegonz@gmail.com>
2023-06-20 07:40:43 -0700

    combinations of 2 digits and 2 digits

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,13 @@ int main(void)
 {
 	int num1, num2, num3, num4;
 
-	num4 = 1;
-
 	for (num1 = 0; num1 <= 9; num1++)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			for (num3 = 0; num3 <= 9; num3++)
+			for (num3 = num1; num3 <= 9; num3++)
 			{
-				for (num4 = num1 + 1; num4 <= 9; num4++)
+				for (num4 = num2 + 1; num4 <= 9; num4++)
 				{
 					putchar(48 + num1);
 					putchar(48 + num2);
