cfcce018bc9370c61296ee1c2682649e9730530d
AaronEGH16 <aezegonz@gmail.com>
2023-06-19 19:27:50 -0700

    print a diagonal with long = N

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "./main.h"
+
+/**
+ * print_diagonal - print diagonal wiht long = N
+ *
+ * @n: number of long
+ */
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	y = 0;
+
+	if (n > 0)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (; y < x; y++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			y++;
+	}
+	_putchar('\n');
+}
