46f0e2070862f00861f12a306205fe6703d72456
AaronEGH16 <aezegonz@gmail.com>
2023-06-19 17:24:32 -0700

    multiply two numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "./main.h"
+
+/**
+ * mul - multiply two numbers
+ *
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: return the multiply of two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
