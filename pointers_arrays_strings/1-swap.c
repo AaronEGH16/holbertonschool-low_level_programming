0e99ca2652d7278180b739a4fcc83588fb4c667a
AaronEGH16 <aezegonz@gmail.com>
2023-06-21 09:12:02 -0700

    swap two values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "./main.h"
+
+/**
+ * swap_int - swap two pointers values are pointing
+ *
+ * @a: pointer to value a
+ * @b: pointer to value b
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
