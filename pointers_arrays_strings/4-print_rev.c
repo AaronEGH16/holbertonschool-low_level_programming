d677374745f3a6624b4c1b0e898657376d38d09e
AaronEGH16 <aezegonz@gmail.com>
2023-06-21 18:32:43 -0700

    print string in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@ void print_rev(char *s)
 {
 	int count = 0;
 
-	while (s[count] != 0)
+	while (*s[count] != 0)
 	{
-		++count;
+		count++;
 	}
 	for (; count >= 0; count--)
 	{
-		_putchar(s[count]);
+		_putchar(*s[count]);
 	}
 	/*_putchar('\n');*/
 }
