02e9323a11d7f823d724858acc545d46fb211342
AaronEGH16 <aezegonz@gmail.com>
2023-06-22 06:06:38 -0700

    inverta a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "./main.h"
+
+/**
+ * rev_string - revert the text in string
+ *
+ * @s: string to invert
+ */
+
+void rev_string(char *s)
+{
+	int count1 = 0;
+	int count2 = 0;
+	char z[];
+
+	while (s[count1] != '\0')
+	{
+		count1++;
+	}
+	while (count1 > 0)
+	{
+		count1--;
+		z[count2] = s[count1];
+		count2++;
+
+	}
+}
