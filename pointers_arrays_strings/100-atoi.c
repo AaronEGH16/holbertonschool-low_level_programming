49567dd2870aec907e29938edc817f5f18d901b8
AaronEGH16 <aezegonz@gmail.com>
2023-06-23 09:00:13 -0700

    get the number on the text and print it

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "./main.h"
+
+/**
+ * _atoi - read teh number in string
+ *
+ * @s: strinng to read
+ */
+
+int _atoi(char *s)
+{
+	int c = 0;
+	int num = 0;
+	int a = 1;
+
+	while (s[c] != '\0')
+	{
+		if (s[c] == '-')
+		{
+			a = a * -1;
+		}
+		if (s[c] >= '0' && s[c] <= '9')
+		{
+			num = num + (s[c] - '0');
+			if (s[c + 1] >= '0' && s[c + 1] <= 57)
+			{
+				num = num * 10;
+			}
+		}
+		if (((s[c] >= '0' && s[c] <= '9') && (s[c + 1] < '0' || s[c + 1] > '9'))
+				|| s[c + 1] == '\0')
+		{
+			return (num * a);
+		}
+		c++;
+	}
+	return (num * a);
+}
