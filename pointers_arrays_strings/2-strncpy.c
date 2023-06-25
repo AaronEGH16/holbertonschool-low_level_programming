cc4b314639f71d609acd1a759464b8fd8fdf6e71
AaronEGH16 <aezegonz@gmail.com>
2023-06-25 10:07:24 -0700

    copy only n chars of the string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "./main.h"
+
+/**
+ * _strncpy - copy the string content to dest but limit string chars
+ *
+ * @dest: destiny of the copy
+ * @src: string to copy
+ * @n: limit chars of the string
+ *
+ * Return: returns a copy of src but only n chars
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int count = 0;
+
+	while (conut < n && src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = '\0';
+	return (dest);
+}
