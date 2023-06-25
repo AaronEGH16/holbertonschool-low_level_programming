6413c1be03cae92e7b81d62497139063674781b5
AaronEGH16 <aezegonz@gmail.com>
2023-06-25 11:15:21 -0700

    copy only n chars of the string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,9 +14,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int count = 0;
 
-	while (count < n)
+	while (count < n && src[count] != '\0')
 	{
 		dest[count] = src[count];
+		count++;
+	}
+	while (count < n)
+	{
+		dest[count] = '\0';
 		count++;
 	}
 
