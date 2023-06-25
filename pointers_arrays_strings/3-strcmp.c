00211cc672feaa17d987632c9279aff2cc766448
AaronEGH16 <aezegonz@gmail.com>
2023-06-25 11:59:46 -0700

    compare two strings and send the diference

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,17 +13,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int count;
 
-	while ((s1[count] == s2[count]) && (s1[count] != '\0'))
+	while (s1[count] != '\0' || s2[count] != '\0')
 	{
+		if (s1[count] != s2[count])
+		{
+			return (s1[count] - s2[count]);
+		}
 		count++;
 	}
-
-	if (s1[count] == s2[count])
-	{
-		return (0);
-	}
-	else
-	{
-		return (s1[count] - s2[count]);
-	}
+	return (0);
 }
