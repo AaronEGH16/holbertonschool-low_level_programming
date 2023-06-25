79f086a47fb911e3996f1ccce6d4590c20d8f34a
AaronEGH16 <aezegonz@gmail.com>
2023-06-25 09:38:51 -0700

    concatenate a two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,10 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int long1, long2, count1, count2, long1_2;
+	int long1, long2;
 
 	long1 = 0;
 	long2 = 0;
-	count1 = 0;
-	count2 = 0;
 
 	while (dest[long1] != '\0')
 	{
@@ -24,24 +22,11 @@ char *_strcat(char *dest, char *src)
 	}
 	while (src[long2] != '\0')
 	{
+		dest[long1] = src[long2];
+		long1++;
 		long2++;
 	}
 
-	long1_2 = long1 + long2;
-
-	char new[long1_2];
-
-	while (count1 < long1)
-	{
-		new[count1] = dest[count1];
-		count1++;
-	}
-	while (count1 < long1_2)
-	{
-		new[count1] = src[count2];
-		count1++;
-		count2++;
-	}
-	new[count1] = '\0';
-	return (new);
+	dest[long1] = '\0';
+	return (dest);
 }
