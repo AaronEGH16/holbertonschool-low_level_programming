839d514d90f54c594320a8d2a01252435aa58c37
AaronEGH16 <aezegonz@gmail.com>
2023-06-26 16:47:39 -0700

    print the sum of diagonals in the square

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "./main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - print the sum of the diagonals of the square
+ *
+ * @a: the square to sum
+ * @size: the size of the square
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int c;
+	int result1 = 0;
+	int result2 = 0;
+
+	for (c = 0; c < size; c++)
+	{
+		result1 += a[c][c];
+	}
+	c = size - 1;
+	for (; c >= 0; c--)
+	{
+		result2 += a[c][c];
+	}
+	printf("%d, %d", result1, result2);
+}
