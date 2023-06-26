565dded3ea06c4ca8585632e85c9f54afe553438
AaronEGH16 <aezegonz@gmail.com>
2023-06-26 06:42:48 -0700

    return a pointer to first ocurrence of c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "./main.h"
+
+/**
+ * _strchr -
+ */
+
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+	while (s[count] != '\0' && s[count] != c)
+	{
+		count++
+	}
+	if (s[count] == c)
+	{
+		return (&s[count]);
+	}
+	return ('\0')
+}
