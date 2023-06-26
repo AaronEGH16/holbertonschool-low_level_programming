c9f4e447cd1c8a7e7a2844627784b1ee6ebc6a4a
AaronEGH16 <aezegonz@gmail.com>
2023-06-26 06:05:13 -0700

    set b in n fields of memory

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "./main.h"
+
+/**
+ * _memset - set b in n fields of memory
+ *
+ * @s: memory to set
+ * @b: value to set in memory
+ * @n: fields of memory to set
+ *
+ * Return: return a point to memory
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (*s < n)
+	{
+		s* = b;
+	}
+	return (*s);
+}
