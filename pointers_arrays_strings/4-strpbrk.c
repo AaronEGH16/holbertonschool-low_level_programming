43a0943ddf8c6b09e0289b6f17fb6063d829fcc8
AaronEGH16 <aezegonz@gmail.com>
2023-06-26 08:37:21 -0700

    return a pointer to the first coincidence of accept in s or null

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "./main.h"
+
+/**
+ * _strpbrk - find the address of the first of any ocurrence and return it
+ *
+ * @s: string where the programs look
+ * @accept: any character to find
+ *
+ * Return: return address to the firs ocurrence of any accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int count = 0;
+	int count2;
+
+	while (s[count] != '\0' && s[count] != c)
+	{
+		for (count2 = 0; accept[count2] != '\0'; count2++)
+		{
+			if (s[count] == accept[count2])
+			{
+				return (&s[count]);
+			}
+		}
+		count++;
+	}
+	return ('\0');
+}
