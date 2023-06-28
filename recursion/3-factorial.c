b16e7740eb75e6b7f5d174618c94cd37ed9a5828
AaronEGH16 <aezegonz@gmail.com>
2023-06-28 09:26:01 -0700

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "./main.h"
+
+/**
+ * factorial - factorize the number N
+ *
+ * @n: int to factorize
+ *
+ * Return: -1 to indicate error, else returns greater than 0
+ */
+
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
