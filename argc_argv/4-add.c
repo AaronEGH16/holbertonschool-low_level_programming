232c5410bd758459f41ed8ec764d475ced5af6bd
AaronEGH16 <aezegonz@gmail.com>
2023-07-01 21:14:31 -0700

    print add of all arguments

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i] <= INT_MAX && argv[i] >= INT_MIN)
+		if (atoi(argv[i]) <= INT_MAX && atoi(argv[i]) >= INT_MIN)
 		{
 			sum += atoi(argv[i]);
 		}
