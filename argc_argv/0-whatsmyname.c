c1273287e63a4b560b2998aa4866d64f829d62a5
AaronEGH16 <aezegonz@gmail.com>
2023-07-01 19:16:28 -0700

    prints executable name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,14 +4,13 @@
 /**
  * main - prints the name of program executable
  *
- * @argc: argument counter
- * @argv: array of argc values
+ * @argv: array of arguments
  *
  * Return: always return 0
  */
 
-int main(int argc, char *argv[])
+int main(char *argv[])
 {
-	printf("%s\n", argv[argc - argc]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
