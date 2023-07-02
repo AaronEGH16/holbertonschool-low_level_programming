f5f535a9aa128b9201b8b9f47e1a7b6d48ec6db1
AaronEGH16 <aezegonz@gmail.com>
2023-07-01 21:05:19 -0700

    print add of all arguments

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -19,19 +19,16 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 0;
 
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		if (isdigit(argv[i]))
 		{
-			if (isdigit(argv[i]))
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
 		}
 	}
 	printf("%d\n", sum);
