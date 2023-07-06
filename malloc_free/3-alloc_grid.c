49ab20c9045d6d68e7da05426e63f4039bea85f5
AaronEGH16 <aezegonz@gmail.com>
2023-07-06 09:35:38 -0700

    grid with memory allocation

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - create a grid with a memory allocation
+ *
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return:
+ *	- pointer to grid
+ *	- NULL if an error has ocurred
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int x, y;
+	char **a;
+
+	if (width <= 0 || height <= 0)
+	{
+		return ('\0');
+	}
+
+	a = malloc(sizeof(int) * (width * height));
+
+	if (!a)
+	{
+		return ('\0');
+	}
+
+	x = 0;
+
+	while (x < height)
+	{
+		y = 0;
+		while (y < width)
+		{
+			a[height][width] = 0;
+			y++;
+		}
+		x++;
+	}
+	return (a);
+}
