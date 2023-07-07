807c725ab18b472669ee49889c34705d80474760
AaronEGH16 <aezegonz@gmail.com>
2023-07-07 16:23:03 -0700

    create a initialized array in memory of nmemb * size (initialized with 0)

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - create a initialized array on memory (initialized with 0)
+ *
+ * @nmemb: number of array slots
+ * @size: size of array slots
+ *
+ * Return:
+ *	- Null if an error has ocurred
+ *	- pointer to array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *a, x;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return ('\0');
+	}
+
+	a = malloc(size * nmemb);
+
+	if (!a)
+	{
+		return ('\0');
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		a[x] = 0;
+	}
+
+	return (a);
+}
