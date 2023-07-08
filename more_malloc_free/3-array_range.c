3813ddabcdc87720564bc8f9cd61ac0d24b88805
AaronEGH16 <aezegonz@gmail.com>
2023-07-07 17:06:06 -0700

    create array of numbers from min to max using malloc

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - create arry of numbers from min to max using malloc
+ *
+ * @min: min number of array
+ * @max: max number of array
+ *
+ * Return:
+ *	- Null if an error has ocurred
+ *	- pointer to range array
+ */
+
+int *array_range(int min, int max)
+{
+	int *a, x;
+
+	if (min > max)
+	{
+		return ('\0');
+	}
+
+	a = malloc(sizeof(int) * (max - min));
+
+	if (!a)
+	{
+		return ('\0');
+	}
+
+	for (x = 0; x < (max - min); x++)
+	{
+		a[x] = min + x;
+	}
+
+	return (a);
+}
