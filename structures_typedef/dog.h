3e5c8e3d4dbe08c177ba817aecb6ff26a515d1ea
AaronEGH16 <aezegonz@gmail.com>
2023-07-10 08:30:46 -0700

    create a struct dog in header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog{
+/**
+ * dog - define a dog item
+ *
+ * @name: the name of the dog
+ * @age: age with the dog have
+ * @owner: name of the dog owner
+ *
+ * Description - all the things a dog has
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
