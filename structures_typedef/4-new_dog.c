60834e6d1afd0930cbe4d7874589b7df21ed23d8
AaronEGH16 <aezegonz@gmail.com>
2023-07-10 10:56:19 -0700

    create a new memory addres of type dog and initalize it with copy name and owner

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,58 @@
 #include "dog.h"
 
 /**
+ * _strlen - get the lenght of string
+ *
+ * @st: string to get lenght
+ *
+ * Return: return the lenght
+ */
+
+int _strlen(char *st)
+{
+	int i = 0;
+
+	if (!st)
+	{
+		return ('\0');
+	}
+
+	while (st[i] != '\0')
+	{
+		i++;
+	}
+	return (i + 1);
+}
+
+/**
+ * _strncpy - copy the string content to dest but limit string chars
+ *
+ * @dest: destiny of the copy
+ * @src: string to copy
+ * @n: limit chars of the string
+ *
+ * Return: returns a copy of src but only n chars
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0;
+
+	while (count < n && src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	while (count < n)
+	{
+		dest[count] = '\0';
+		count++;
+	}
+
+	return (dest);
+}
+
+/**
  * new_dog - create a new slot in memory of type dog and initalize it
  *
  * @name: name to initialize dog
@@ -16,16 +68,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *copy_name;
+	char *copy_owner;
 
 	d = malloc(sizeof(dog_t));
 	if (!d)
 	{
+		free(d);
 		return ('\0');
 	}
 
-	d->name = name;
+	_strncpy(copy_name, name, _strlen(name));
+	_strncpy(copy_owner, owner, _strlen(owner));
+
+	d->name = copy_name;
 	d->age = age;
-	d->owner = owner;
+	d->owner = copy_owner;
 
 	return (d);
 }
