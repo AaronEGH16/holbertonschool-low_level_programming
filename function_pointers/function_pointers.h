2b2ef9810d40ce24a8540aa43da26685c80e07ab
AaronEGH16 <aezegonz@gmail.com>
2023-07-11 08:47:36 -0700

    header file

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,9 +2,6 @@
 #define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
-
-typedef unsigned int size_t;
-
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
