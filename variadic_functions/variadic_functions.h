5c4d635dce39fc7394db782b23ae8ecef19a5116
AaronEGH16 <aezegonz@gmail.com>
2023-07-13 15:29:18 -0700

    header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,4 +6,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+typedef struct op
+{
+	char *op;
+	void (*f)(void a);
+} op_t;
+
+void pr_char((int)a);
+void pr_int((int)a);
+void pr_float((double)a);
+void pr_string((char*)a);
+
 #endif
