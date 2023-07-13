98f68c5aa0a1656880114896e8a8443a990a7f7b
AaronEGH16 <aezegonz@gmail.com>
2023-07-12 17:24:22 -0700

    print all args strings with a separator

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints a strings arguments with a separator
+ *
+ * @separator: separator of the strings arguments
+ * @n: number of strings to print
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list psl;
+	char *str;
+	unsigned int i = 0;
+
+	va_start(psl, n);
+
+	while (i < n)
+	{
+		str = va_arg(pnl, (char *));
+
+		printf("%s", ((str == NULL) ? "(nil)" : str));
+		if (separator && i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+
+		i++;
+	}
+	printf("\n");
+
+	va_end(psl);
+}
