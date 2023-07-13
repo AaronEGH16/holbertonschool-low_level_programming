4facfedd9d4cf113d96f169bd1a70399dd88768b
AaronEGH16 <aezegonz@gmail.com>
2023-07-12 17:01:46 -0700

    print all args number but separtor

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdrag.h>
+
+/**
+ * print_numbers - print all numbers passed by arguments with separator
+ *
+ * @separator: separator of numbers
+ * @n: number of numbers to print
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list pnl;
+
+	unsigned int i = 0;
+
+	va_start(pnl, n);
+
+	while (i < n)
+	{
+		printf("%d", va_arg(pnl, unsigned int));
+		if (separator && i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+
+	va_end(pnl);
+}
