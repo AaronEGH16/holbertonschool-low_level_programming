146860cd75d13ea1c82f56d0df4530e2ffb8aee4
AaronEGH16 <aezegonz@gmail.com>
2023-07-26 06:30:43 -0700

    rewrite all function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,36 +9,32 @@
  *
  * Return:
  *		- NULL if an error has ocurred
- *		- list with a new content
+ *		- addres of the new content
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *aux = *h;
 	dlistint_t *new;
+	unsigned int num;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (; idx != 1; idx--)
+	for (num = 0; num < idx; num++)
 	{
 		aux = aux->next;
-		if (aux == NULL)
+		if (aux->next == NULL)
 			return (NULL);
 	}
 
 	if (aux->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	new = malloc(sizeof(dlistint_t));
-		if (!new)
-			return (NULL);
-
 	new->n = n;
 	new->prev = aux;
 	new->next = aux->next;
-	aux->next->prev = new;
 	aux->next = new;
 
-	return (aux);
+	return (new);
 }
