f8d6b24ac3bc1f6e96a7fb1b524262afbe2e86fa
AaronEGH16 <aezegonz@gmail.com>
2023-07-26 05:52:45 -0700

    insert a new node in index location

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert a new node in the index location
+ *
+ * @h: list
+ * @idx: location to insert
+ * @n: content of the node
+ *
+ * Return:
+ *		- NULL if an error has ocurred
+ *		- list with a new content
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *aux = *h;
+	dlistint_t *new;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (idx != 1)
+	{
+		aux = aux->next;
+		if (!aux)
+			return (NULL);
+		idx--;
+	}
+
+	if (!(aux->next))
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+		if (!new)
+			return (NULL);
+	new->next = aux->next;
+	new->prev = aux;
+	new->n = n;
+	aux->next = new;
+
+	return (*h);
+}
