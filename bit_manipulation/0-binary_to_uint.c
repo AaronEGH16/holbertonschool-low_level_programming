4c5abe33884ef18bed79cc8fda1572332f8bc725
AaronEGH16 <aezegonz@gmail.com>
2023-07-28 08:37:14 -0700

    function to get an binary string num and convert in to unsigned int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * binary_to_uint - get a binary string and convert in to unsigned int
@@ -17,14 +18,14 @@ unsigned int binary_to_uint(const char *b)
 	int exp = strlen(b);
 	int rec = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
 	while (b[rec] != '\0')
 	{
 		if (b[rec] < '0' || b[rec] > '1')
 			return (0);
-		res += (2 ^ exp) * b[rec];
+		res += (2 ^ exp) * atoi(b[rec]);
 		rec++, exp--;
 	}
 
