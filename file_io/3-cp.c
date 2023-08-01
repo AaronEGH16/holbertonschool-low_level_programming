65f7fac6bb97c5bc517aa0b4625ed218d8313024
AaronEGH16 <aezegonz@gmail.com>
2023-08-01 16:32:34 -0700

    function to copy a file content in a new file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -28,18 +28,19 @@ int main(int ac, char *av[])
 	if (fd1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	fr = read(fd1, buf, 1024);
-	if (fr == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
 	fd2 = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	fw = write(fd2, buf, 1024);
-	if (fw == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	while ((fr = read(fd1, buf, 1024)) != 0)
+	{
+		if (fr == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
+		fw = write(fd2, buf, 1024);
+		if (fw == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
 	fc = close(fd2);
 	if (fc == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
