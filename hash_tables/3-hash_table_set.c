80066dbdbdf09068e78ff9b3e0c4de116ddcaa0e
AaronEGH16 <aezegonz@gmail.com>
2023-08-03 08:35:48 -0700

    function to add new value to hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	idx = key_index((unsigned char *) key, ht->size);
 	srch = ht->array[idx];
 
-	if (srch && strcmp(key, elem->key) == 0)
+	if (srch && strcmp(key, srch->key) == 0)
 	{
 		free(srch->value);
 		srch->value = strdup(value);
