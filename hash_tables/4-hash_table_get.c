db3fa110c6579cbdd2ba7fe0584501271fcc7666
AaronEGH16 <aezegonz@gmail.com>
2023-08-03 09:45:07 -0700

    function to get value associated to key

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get value associated a key in the hash table
+ *
+ * @ht: addres of a hash table
+ * @key: ...
+ *
+ * Return:
+ *		- value associated to key
+ *		- NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *srch = NULL;
+
+	idx = key_index((unsigned char *)key, ht->size);
+
+	srch = ht->array[idx];
+
+	if (!srch)
+		return (NULL);
+
+	while (srch && key != srch->key)
+		srch = srch->next;
+
+	if (!srch)
+		return (NULL);
+
+	return (srch->value);
+}
